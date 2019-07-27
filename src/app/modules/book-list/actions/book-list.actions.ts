0f2100c3267986afb16ada0447c8d3790854b61d
Meitix <m.valizadeh@live.com>
2019-07-27 18:13:01 +0800

    immer package added.

--- src/app/modules/book-list/actions/book-list.actions.ts
+++ src/app/modules/book-list/actions/book-list.actions.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Action } from '@ngrx/store';
-import { IBook } from '../../../models/book.interface';
 
 export const RATE_BOOK = '[BOOK] Rate';
 export const ORDER_BOOK_LIST = '[BOOK] Order';
 
 export class RateBook implements Action {
     readonly type = RATE_BOOK;
-    constructor(public payload: IBook) {}
+    constructor(public payload: {index: number , rate: number}) {}
 }
 
-export type BookActions = RateBook;
+export type BookListActions = RateBook;
---
+++ src/app/modules/book-list/reducers/book-list.reducer.spec.ts
@@ -0,0 +1,5 @@
+describe('Book list reducer' , () => {
+    it('should set the new rate to the item with same index.', () => {
+         
+    });
+});
