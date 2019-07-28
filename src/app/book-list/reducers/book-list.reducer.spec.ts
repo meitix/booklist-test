da0770877ad5c2206696d3401a3611b5cd6de440
Meitix <m.valizadeh@live.com>
2019-07-28 14:31:18 +0800

    Rate book reducer test added.

--- src/app/book-list/reducers/book-list.reducer.spec.ts
+++ src/app/book-list/reducers/book-list.reducer.spec.ts
@@ -1,3 +1,23 @@
-describe('Book list reducer', () => {
-  it('should set the new rate to the item with same index.', () => {});
+import { RateBook } from '../actions/book-list.actions';
+import { BookListReducer } from './book-list.reducer';
+const state = [
+  { title: 'harry potter', rate: 0 },
+  { title: 'harry potter 2', rate: 1 }
+];
+describe('Rate Book', () => {
+
+  it('Should not manipulate the current state object after creating new state', () => {
+    const rateAction = new RateBook({ index: 1, rate: 5 });
+    const newState = BookListReducer(state, rateAction);
+    expect(newState).not.toEqual(state);
+  });
+
+  it('should set the new rate to the item with same index.', () => {
+    const rateAction = new RateBook({ index: 0, rate: 5 });
+    const newState = BookListReducer(state, rateAction);
+    expect(newState[rateAction.payload.index].rate).toEqual(rateAction.payload.rate);
+  });
 });
+
+
+
