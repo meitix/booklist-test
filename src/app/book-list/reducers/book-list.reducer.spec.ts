ca2709e5c848d73fb7d944172808a474d6e75ca9
Meitix <m.valizadeh@live.com>
2019-07-28 18:59:12 +0800

    Ordering the list test added.

--- src/app/book-list/reducers/book-list.reducer.spec.ts
+++ src/app/book-list/reducers/book-list.reducer.spec.ts
@@ -1,7 +1,7 @@
-import { RateBook, LoadData } from '../actions/book-list.actions';
+import { RateBook, LoadData, OrderData } from '../actions/book-list.actions';
 import { BookListReducer } from './book-list.reducer';
 import { IBook } from 'src/app/models/book.interface';
-const state = [
+let state = [
   { title: 'harry potter', rate: 0 },
   { title: 'harry potter 2', rate: 1 }
 ];
@@ -33,6 +33,31 @@ describe('Initial data', () => {
   it('Should put the provided data to state', () => {
     const loadData = new LoadData(data);
     const newState = BookListReducer(state, loadData);
-    expect(newState).toEqual(data);
+    // use new data if test passes successfully.
+    if (expect(newState).toEqual(data)) {
+      state = newState;
+    }
   });
+});
+
+describe('Ordering the list', () => {
+  it('should order the array based on item rates', () => {
+    const orderData = new OrderData({ property: 'rate', orderType: 'desc' });
+    const orderedBooks = BookListReducer(state, orderData);
+    // check array is sorted or not.
+    for (let i = 0; i < orderedBooks.length; i++) {
+      if (i + 1 !== orderedBooks.length) {
+        expect(orderedBooks[i].rate).toBeGreaterThanOrEqual(
+          orderedBooks[i + 1].rate
+        );
+      }
+    }
+  });
+});
+
+// array length test.
+it('Ordered array should have same length as unordered array after sorting.', () => {
+  const orderData = new OrderData({ property: 'rate', orderType: 'desc' });
+  const orderedBooks = BookListReducer(state, orderData);
+  expect(orderedBooks.length).toEqual(state.length);
 });
