8a73be9f80eb9cf52d881ab2862be3031eb97b74
Meitix <m.valizadeh@live.com>
2019-07-29 00:51:08 +0800

    load data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from './book-list/services/book.service';
+import { BOOKS } from './data/books.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'booklist-test';
+  constructor(private bookService: BookService) {}
+ngOnInit() {
+this.bookService.loadData(BOOKS);
+}
 }
